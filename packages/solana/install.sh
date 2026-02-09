604ee170de2ca7e8cf849d42d7e106a6cd12fb7e
Robert Roach <rjayroach@gmail.com>
2026-02-09 18:18:48 +0800

    more targeted solana install

--- packages/solana/install.sh
+++ packages/solana/install.sh
@@ -4,8 +4,28 @@ dependencies() {
   echo "rust node"
 }
 
-post_install() {
+x_post_install() {
   source <(mise activate bash)
   curl --proto '=https' --tlsv1.2 -sSfL https://solana-install.solana.workers.dev | bash
+}
+
+
+# From https://solana.com/docs/intro/installation/dependencies
+post_install() {
+  source <(mise activate bash)
+
+  # Solana CLI
+  sh -c "$(curl -sSfL https://release.anza.xyz/stable/install)"
+
+  # Anchor
+  cargo install --git https://github.com/coral-xyz/anchor avm --force
+  avm install latest
+  avm use latest
+
+  # Update the Solana CLI to the latest version, as needed (Optional)
+  # agave-install update
+
+  # surfpool
+  curl -sL https://run.surfpool.run/ | bash
 }
 
