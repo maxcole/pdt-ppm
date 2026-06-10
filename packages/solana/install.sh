216922a060b171d0369c582b8ec12c39ae3a2811
Robert Roach <rjayroach@gmail.com>
2026-06-10 10:21:31 +0800

    tighten up solana install; add zsh completions for cli, anchor and surfpool

--- packages/solana/install.sh
+++ packages/solana/install.sh
@@ -7,36 +7,53 @@ install_linux() {
   fi
 }
 
+# NOTE: Not using brew for surfpool since post_install uses a cross platform installer, but here in case future want to use packagges; Linux can use `snap`
+# install_macos() {
+#   # From https://youtu.be/tibT9ZPb8wE?list=PL0FMgRjJMRzO1FdunpMS-aUS4GNkgyr3T&t=19
+#   brew tap txtx/taps
+#   install_dep surfpool
+# }
+
 # From https://solana.com/docs/intro/installation/dependencies
 post_install() {
-  source <(mise activate bash)
-  install_completion "solana completion -s zsh"
+  # 1. Solana CLI
+  if command -v agave-install &> /dev/null; then
+    # Update the Solana CLI to the latest version
+    agave-install update
+  else
+    sh -c "$(curl -sSfL https://release.anza.xyz/stable/install)"
+  fi
+  install_completion "solana completion --shell zsh"
 
-  # Solana CLI
-  sh -c "$(curl -sSfL https://release.anza.xyz/stable/install)"
+  # 2. Anchor Version Manager (avm)
+  if command -v avm &> /dev/null; then
+    avm self-update
+  else
+    cargo install --git https://github.com/coral-xyz/anchor avm --force
+  fi
 
-  # Anchor
-  cargo install --git https://github.com/coral-xyz/anchor avm --force
+  # 3. Anchor Framework
   avm install latest
   avm use latest
+  install_completion "anchor completions zsh"
 
-  # Update the Solana CLI to the latest version, as needed (Optional)
-  # agave-install update
-
-  # surfpool
+  # 4. Surfpool local validator
   curl -sL https://run.surfpool.run/ | bash
+  # Surfpool does not output to STDOUT, but rather writes a file
+  surfpool completions zsh
+  mv _surfpool $PPM_FPATH
 }
 
 post_remove() {
-  # Solana CLI (installed via Anza installer)
+  # 1. Solana CLI (installed via Anza installer)
   rm -rf "$XDG_DATA_HOME/solana"
   rm -rf "$XDG_CACHE_HOME/solana"
 
-  # Anchor/AVM
+  # 2. Anchor/AVM
   cargo uninstall avm 2>/dev/null || true
   rm -rf "$HOME/.avm"
 
-  # Surfpool
+  # 3. Surfpool
   rm -f "$BIN_DIR/surfpool"
 
   if $force; then
