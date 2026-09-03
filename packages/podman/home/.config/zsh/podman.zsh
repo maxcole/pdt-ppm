222fe22af893093f4286be973e15260d2afa53c2
Robert Roach <rjayroach@gmail.com>
2026-09-03 17:41:39 -0400

    add zsh aliases/functions

--- packages/podman/home/.config/zsh/podman.zsh
+++ packages/podman/home/.config/zsh/podman.zsh
@@ -6,6 +6,13 @@ alias docker=podman
 alias dcd="docker compose down"
 alias dvls="docker volume ls"
 
+alias pd="podman-compose down"
+alias pps="podman ps"
+alias ppsa="pps --all"
+alias prm="podman rm"
+alias pu="podman-compose up"
+alias pud="pu -d"
+
 # disables podman compose notification
 export PODMAN_COMPOSE_WARNING_LOGS=false
 
--- packages/solana/home/.config/zsh/solana.zsh
+++ packages/solana/home/.config/zsh/solana.zsh
@@ -1,5 +1,22 @@
 # solana.zsh
 
-
 # Add solana release path to the search path
 ensure_path "$XDG_DATA_HOME/solana/install/active_release/bin"
+
+# grind out vanity keypair(s) starting with <param>
+solkg() {
+    # Check if the first required parameter is missing
+    if [[ -z "$1" ]]; then
+        echo "Error: You must provide a prefix."
+        echo "Usage: skg <prefix> [num_of_keys]"
+        return 1
+    fi
+
+    # Set the second parameter, defaulting to 1 if not provided
+    local num_keys="${2:-1}"
+
+    echo "Starting Solana grind for prefix '$1' (Target: $num_keys key(s))..."
+
+    # Run the solana-keygen command
+    solana-keygen grind --starts-with "$1:$num_keys"
+}
